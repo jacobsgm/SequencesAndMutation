e7e26f0297137d289e02540d0b59eb52522f8053
jacobsgm <jacobsgm@rose-hulman.edu>
2018-01-17 14:27:36 -0500

    this is rough

--- src/m2_more_sequences.py
+++ src/m2_more_sequences.py
@@ -330,7 +330,7 @@ def is_palindrome(s):
       :type s: str
     """
     # ------------------------------------------------------------------
-    # TODO: 5. Implement and test this function.
+    # DONE: 5. Implement and test this function.
     #     The testing code is already written for you (above).
     #
     ####################################################################
@@ -341,10 +341,9 @@ def is_palindrome(s):
     # ------------------------------------------------------------------
 
 
-    for k in range((len(s))/2):
-        if(s[k] != s[len(s)-k]):
+    for k in range(int((len(s))/2)):
+        if(s[k] != s[len(s)-k-1]):
             return False
-
     return True
 
 # ----------------------------------------------------------------------
@@ -403,9 +402,15 @@ def count_same(sequence1, sequence2):
       type: sequence2: tuple or list or string
     """
     # ------------------------------------------------------------------
-    # TODO: 6. Implement and test this function.
+    # DONE: 6. Implement and test this function.
     #     The testing code is already written for you (above).
     # ------------------------------------------------------------------
+
+    count = 0
+    for k in range(len(sequence1)):
+        if (sequence1[k] == sequence2[k]):
+            count+=1
+    return count
 
 
 # ----------------------------------------------------------------------
